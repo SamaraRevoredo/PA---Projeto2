339353b2bbc3dbd9159931ec9eccd1a25e0525b5
Samara Revoredo <34380412+SamaraRevoredo@users.noreply.github.com>
2019-10-18 03:31:33 -0300

    Add files via upload

---
+++ search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "cdfmp",
+  1: "cfp",
+  2: "cfmp",
+  3: "cdfmp"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Classes",
+  2: "Arquivos",
+  3: "Funções"
+};
+
